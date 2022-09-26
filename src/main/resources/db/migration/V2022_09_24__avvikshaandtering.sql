60249d2e20455036cbcbdf957732f789e0af60d7
Jone Lura <jone.lura@gmail.com>
2022-09-26 10:41:43 +0200

    YSMOD-597
    - endret på dato og sortering

--- src/main/resources/db/migration/V2022_09_24__avvikshaandtering.sql
+++ src/main/resources/db/migration/V2022_09_24__avvikshaandtering.sql
@@ -1,16 +1,16 @@
 -- Kodelisten 'Dokumenttyper'
 INSERT INTO type (navn, beskrivelse, beskyttet) VALUES ('aarsakAvvik', 'Årsaker for Avvik', true);
 
-INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('feilInnhold', '2022-08-04', null);
-INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('uleseligDokument', '2022-08-04', null);
-INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('annenPerson', '2022-08-04', null);
-INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('inneholderVedlegg', '2022-08-04', null);
+INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('feilInnhold', '2022-09-26', null);
+INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('uleseligDokument', '2022-09-26', null);
+INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('annenPerson', '2022-09-26', null);
+INSERT INTO kode (kode, gyldig_fra, gyldig_til) VALUES ('inneholderVedlegg', '2022-09-26', null);
 
 INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'feilInnhold', 100);
-INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'uleseligDokument', 100);
-INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'annenPerson', 100);
-INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'inneholderVedlegg', 100);
-INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'annet', 200);
+INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'uleseligDokument', 200);
+INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'annenPerson', 300);
+INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'inneholderVedlegg', 400);
+INSERT INTO type_kode (type_navn, kode, sortering) VALUES ('aarsakAvvik', 'annet', 1000);
 
 -- Bokmål
 INSERT INTO verdi (kode, spraak, verdi) VALUES ('feilInnhold', 'nb', 'Feil innhold i dokumentet');
